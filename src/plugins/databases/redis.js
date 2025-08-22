ff9d406d4896e26b20a4281bff3eff3d5e11e1db
Bruno Lopes <bruno-lopes9@hotmail.com>
2025-08-22 18:36:51 +0100

    a

--- src/plugins/databases/redis.js
+++ src/plugins/databases/redis.js
@@ -1,27 +1,23 @@
 import fp from "fastify-plugin";
 import { createClient } from "redis";
 
-const client = createClient({ url: process.env.REDIS_URL });
-await client.connect();
-await client.set("key", "node redis");
-const value = await client.get("key");
-console.log(value);
-
 async function redisPlugin(fastify, config) {
-  const client = createClient({ url: process.env.REDIS_URL });
+  const client = createClient({ url: config.url });
 
   client.on("error", (err) => fastify.log.error("Redis Client Error:", err));
 
   try {
-    await client.connect(); // garante que a conexão ocorreu
-    await client.ping(); // opcional, só pra ter certeza
+    await client.connect(); // Se falhar, loga erro
+    await client.ping();
     fastify.log.info("Connected to Redis");
   } catch (err) {
     fastify.log.error("Failed to connect to Redis:", err);
-    throw err; // evita timeout no session-plugin
+    fastify.decorate("redisStatus", "error"); // evita travar plugin
+    return; // não lança erro, evita timeout
   }
 
   fastify.decorate("redis", client);
+  fastify.decorate("redisStatus", "connected");
 
   fastify.addHook("onClose", async () => {
     await client.quit();
