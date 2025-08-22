aafe59f06ee8ee40666da476614d046fd7507b78
Bruno Lopes <bruno-lopes9@hotmail.com>
2025-08-22 18:17:14 +0100

    a

--- src/plugins/databases/redis.js
+++ src/plugins/databases/redis.js
@@ -2,34 +2,30 @@ import fp from "fastify-plugin";
 import { createClient } from "redis";
 
 async function redisPlugin(fastify, config) {
-  let redisStatus = "disconnected";
+  const client = createClient({
+    username: config.username,
+    password: config.password,
+    socket: {
+      host: config.socket.host,
+      port: config.socket.port
+    }
+  });
+
+  client.on("error", (err) => fastify.log.error("Redis Client Error:", err));
 
   try {
-    const client = createClient({ url: config.url });
-
-    client.on("ready", () => {
-      redisStatus = "connected";
-      fastify.log.info("Redis client ready");
-    });
-
-    client.on("error", (err) => {
-      redisStatus = "error";
-      fastify.log.error("Redis Client Error:", err);
-    });
-
-    await client.connect();
-
-    fastify.decorate("redis", client);
-    fastify.decorate("redisStatus", () => redisStatus);
+    await client.connect(); // garante que a conexão ocorreu
+    await client.ping(); // opcional, só pra ter certeza
+    fastify.log.info("Connected to Redis");
   } catch (err) {
     fastify.log.error("Failed to connect to Redis:", err);
-    throw err;
+    throw err; // evita timeout no session-plugin
   }
 
-  fastify.addHook("onClose", async (fastifyInstance, done) => {
-    redisStatus = "disconnected";
-    await fastify.redis.quit();
-    done();
+  fastify.decorate("redis", client);
+
+  fastify.addHook("onClose", async () => {
+    await client.quit();
   });
 }
 
